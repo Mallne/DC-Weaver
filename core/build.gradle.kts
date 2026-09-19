b38a253329eed1b7534912085a5cc0a1b4bc0fd6
mallne <info@mallne.cloud>
2026-09-19 20:16:19 +0200

    chore: hardcode version to 1.0.0-SNAPSHOT, tag drives releases via -PVERSION_NAME

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 group = "cloud.mallne.dicentra.weaver"
-version = project.findProperty("VERSION_NAME") ?: "1.0.0-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 description = "The core compponents of DiCentra Weaver."
 
 plugins {
